5a5a8e1c24f174fe57ddb00fe7f245df478cb3d1
Brandon <brandonpretelt93@gmail.com>
2021-10-19 20:51:38 -0600

    :sparkles: new-ish layout

--- build/films/app.js
+++ build/films/app.js
@@ -1,34 +1,74 @@
 import { films } from '../data/films.js';
-const rootApp = document.getElementById('App');
-const container = document.createElement('div');
-container.textContent = 'Hello world';
-container.classList.add('container');
-rootApp.append(container);
-// const filmsContainer = document.querySelector('.films-container');
 
-// films.forEach((film, index) => {
-//     const h1El = document.createElement('h1');
-//     const imgEl = document.createElement('img');
-//     const divEl = document.createElement('div');
-//     const sw_card = document.createElement('div');
-//     const pEl = document.createElement('p');
-//     const { title } = film;
-//     // .card, .card-title, .card-content
-//     h1El.textContent = title;
-//     h1El.className = 'card-title';
-//     pEl.className = 'characters';
-//     imgEl.src = `https://starwars-visualguide.com/assets/img/films/${
-//         index + 1
-//     }.jpg`;
-//     divEl.append(h1El, imgEl);
-//     divEl.classList.add(`film-${index}`);
-//     divEl.classList.add('card-content');
-//     divEl.appendChild(pEl);
+const app = (data) => {
+    const rootApp = document.getElementById('App');
+    const container = document.createElement('div');
+    const mainHeader = document.createElement('header');
+    const mainHeaderHeading = document.createElement('h1');
 
-//     // append card-content and add a class of card
-//     sw_card.classList.add('card');
-//     sw_card.appendChild(divEl);
+    mainHeaderHeading.textContent = 'Star Wars Films';
+    mainHeader.append(mainHeaderHeading);
+    container.classList.add('container');
 
-//     // append card to main filmscontainer
-//     filmsContainer.append(sw_card);
+    data.forEach((d, index) => {
+        const headerElement = document.createElement('header');
+        const h1Element = document.createElement('h1');
+        const divElement = document.createElement('div');
+        const imgElement = document.createElement('img');
+
+        const divCrawlElement = document.createElement('div');
+        const crawlPElement = document.createElement('p');
+
+        divElement.classList.add('card');
+        headerElement.classList.add('card-title');
+        divCrawlElement.classList.add('card-content');
+
+        h1Element.textContent = d.title;
+        crawlPElement.textContent = d.opening_crawl;
+        imgElement.src = `https://starwars-visualguide.com/assets/img/films/${
+            index + 1
+        }.jpg`;
+
+        headerElement.append(h1Element);
+        divElement.append(headerElement);
+        divElement.append(imgElement);
+        divCrawlElement.append(crawlPElement);
+        divElement.append(divCrawlElement);
+        container.append(divElement);
+    });
+    rootApp.append(mainHeader, container);
+};
+
+// NOPE to below code
+
+// const newArr = [];
+// films.forEach((film) => {
+//     newArr.push(film.episode_id);
 // });
+
+// const sortedArr = newArr;
+// sortedArr.sort((a, b) => {
+//     return a - b;
+// });
+// console.log(sortedArr);
+
+// filmsEpisodes.sort((a, b) => {
+//     console.log(a - b);
+// });
+
+// const findByFilmNumber = (arr) => {
+//     const OT = [];
+//     const PT = [];
+//     const ST = [];
+//     let title;
+//     arr.find((element) => {
+//         if (element.url.includes(1)) {
+//             OT.push(element.title);
+//         }
+//     });
+//     title = OT;
+//     return title;
+// };
+
+// findByFilmNumber(films);
+app(films);
