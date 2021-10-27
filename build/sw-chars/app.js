d986c5e6ada63fda8ae1b14190b68676fe1891e6
Brandon <brandonpretelt93@gmail.com>
2021-10-26 23:13:43 -0600

    :sparkles: added btns to filter, it's meh

--- build/sw-chars/app.js
+++ build/sw-chars/app.js
@@ -7,8 +7,56 @@ const app = (data) => {
     const container = document.createElement('div');
     const mainHeader = document.createElement('header');
     const mainHeaderHeading = document.createElement('h1');
+    const maleBtn = document.createElement('button');
+    const femaleBtn = document.createElement('button');
+
+    femaleBtn.textContent = 'Female Characters';
+    maleBtn.textContent = 'Male Characters';
+
+    rootApp.prepend(femaleBtn);
+    rootApp.prepend(maleBtn);
+
+    const isolateID = (item) => {
+        // refactor using lastindexof and chart and end - 2;
+
+        let id;
+        id = item.url.slice(item.url.length - 3);
+        let newId = id.slice(0, -1);
+        if (newId[0] === '/') {
+            return (newId = newId.slice(1));
+        } else {
+            return (newId = id.slice(0, -1));
+        }
+    };
+
+    const searchMaleFemale = (g) => {
+        const divElement = document.createElement('div');
+        divElement.classList.add('card');
+        const divClear = document.querySelectorAll('.card');
+        // const headerElement = document.createElement('header');
+        // const h1Element = document.createElement('h1');
+        console.log(divElement);
+        people.filter((person) => {
+            if (g === person.gender) {
+                console.log(person.gender);
+                return (divElement.innerHTML += `
+                <header class='card-title'>
+                <h1>${person.name}</h1>
+                <img src='https://starwars-visualguide.com/assets/img/characters/${isolateID(
+                    person
+                )}.jpg'>
+                    </header>
+                    `);
+            }
+        });
+        divClear.innerHTML = '';
+    };
+    maleBtn.addEventListener('click', () => searchMaleFemale('male'));
+    // searchMaleFemale('male');
+    // searchMaleFemale('female');
 
     data.forEach((person) => {
+        // rewrite as a function, characters as param
         const headerElement = document.createElement('header');
         const divElement = document.createElement('div');
         const h1Element = document.createElement('h1');
@@ -20,18 +68,10 @@ const app = (data) => {
         headerElement.classList.add('card-title');
         divInfoElement.classList.add('card-content');
         h1Element.innerHTML = person.name;
-        const isolateID = () => {
-            let id;
-            id = person.url.slice(person.url.length - 3);
-            let newId = id.slice(0, -1);
-            if (newId[0] === '/') {
-                return (newId = newId.slice(1));
-            } else {
-                return (newId = id.slice(0, -1));
-            }
-        };
 
-        imgElement.src = `https://starwars-visualguide.com/assets/img/characters/${isolateID()}.jpg`;
+        imgElement.src = `https://starwars-visualguide.com/assets/img/characters/${isolateID(
+            person
+        )}.jpg`;
 
         headerElement.append(h1Element);
         divElement.append(headerElement);
