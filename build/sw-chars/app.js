68aab6d79bdd798223300ec6fa4208844f576f45
Brandon <brandonpretelt93@gmail.com>
2021-10-30 16:54:47 -0600

    :recycle: refactor is a wip

--- build/sw-chars/app.js
+++ build/sw-chars/app.js
@@ -1,57 +1,57 @@
 import { people } from '../data/people.js';
 
-const populateDOM = (arr) => {
-    const rootApp = document.querySelector('#App');
-    const container = document.createElement('div');
-    const mainHeader = document.createElement('header');
-    const mainHeaderHeading = document.createElement('h1');
-    const maleBtn = document.createElement('button');
-    const femaleBtn = document.createElement('button');
+const rootApp = document.querySelector('#App');
+const container = document.createElement('div');
+const mainHeader = document.createElement('header');
+const mainHeaderHeading = document.createElement('h1');
+const maleBtn = document.createElement('button');
+const femaleBtn = document.createElement('button');
 
+const populateDOM = (arr) => {
     femaleBtn.textContent = 'Female Characters';
     maleBtn.textContent = 'Male Characters';
 
-    rootApp.prepend(femaleBtn);
-    rootApp.prepend(maleBtn);
+    rootApp.prepend(maleBtn, femaleBtn);
+
+    const grabLast = (item) => {
+        return item.url.slice(item.url.length - 3);
+    };
 
     const isolateID = (item) => {
-        // refactor using lastindexof and chart and end - 2;
-
         let id;
-        id = item.url.slice(item.url.length - 3);
+        id = grabLast(item);
         let newId = id.slice(0, -1);
         if (newId[0] === '/') {
             return (newId = newId.slice(1));
-        } else {
-            return (newId = id.slice(0, -1));
         }
+        return newId;
     };
 
-    const searchMaleFemale = (g) => {
-        const divElement = document.createElement('div');
-        divElement.classList.add('card');
-        const divClear = document.querySelectorAll('.card');
-        // const headerElement = document.createElement('header');
-        // const h1Element = document.createElement('h1');
-        console.log(divElement);
-        people.filter((person) => {
-            if (g === person.gender) {
-                console.log(person.gender);
-                return (divElement.innerHTML += `
-                <header class='card-title'>
-                <h1>${person.name}</h1>
-                <img src='https://starwars-visualguide.com/assets/img/characters/${isolateID(
-                    person
-                )}.jpg'>
-                    </header>
-                    `);
-            }
-        });
-        divClear.innerHTML = '';
-    };
-    maleBtn.addEventListener('click', () => searchMaleFemale('male'));
-    // searchMaleFemale('male');
-    // searchMaleFemale('female');
+    // const render = (element) => {
+    //     return (element.innerHTML += `
+    //     <header class='card-title'>
+    //     <h1>${person.name}</h1>
+    //     <img src='https://starwars-visualguide.com/assets/img/characters/${isolateID(
+    //         person
+    //     )}.jpg'>
+    //         </header>
+    //         `);
+    // };
+
+    // const searchMaleFemale = (g) => {
+    //     const divElement = document.createElement('div');
+    //     divElement.classList.add('card');
+    //     const divClear = document.querySelectorAll('.card');
+    //     console.log(divElement);
+    //     people.filter((person) => {
+    //         if (g === person.gender) {
+    //             console.log(person.gender);
+    //         }
+    //         render(divElement);
+    //     });
+    //     divClear.innerHTML = '';
+    // };
+    // maleBtn.addEventListener('click', () => searchMaleFemale('male'));
 
     arr.forEach((person) => {
         // rewrite as a function, characters as param
