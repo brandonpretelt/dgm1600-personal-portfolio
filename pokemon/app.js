97266bb4fe88434d410f3bbe3ce3b85934e7c432
Brandon <brandonpretelt93@gmail.com>
2021-12-01 21:20:58 -0700

    :hammer: added styles for cards

--- pokemon/app.js
+++ pokemon/app.js
@@ -1,32 +1,14 @@
 import { addListeners } from '../utils/utils.js';
 
 const pokemonLimit = document.getElementById('enter-pokemon-limit');
-const offsetNumber = document.getElementById('enter-offset-number');
 const btn = document.getElementById('get-pokemon-data');
 
 const apiUrl = `https://pokeapi.co/api/v2/pokemon/?limit=`;
 
-// btn.addEventListener('click', () => {
-
-// });
-// btn.addEventListener('click', () => {
-//     fetch(`${apiUrl}${pokemonLimit.value}`)
-//         .then((res) => res.json())
-//         .then((data) => {
-//             const { results } = data;
-//             // console.log(results);
-//             for (const pokemon of results) {
-//                 fetch(pokemon.url)
-//                     .then((res) => res.json())
-//                     .then((data) => console.log(data.id));
-//             }
-//         });
-// });
 const getData = (url) => {
     try {
-        return fetch(url, { cache: 'force-cache' })
-            .then((res) => res.json())
-            .then((data) => data);
+        return fetch(url).then((res) => res.json());
+        // .then((data) => data);
     } catch (e) {
         console.log(e, ' error message of sorts');
     }
@@ -35,68 +17,34 @@ const getData = (url) => {
 addListeners(btn, 'click', () => {
     getData(`${apiUrl}${pokemonLimit.value}`).then((data) => {
         let container;
-        container = document.createElement('div');
-        // console.log(typeof data);
-        // const results = Promise.all(data.results);
-        // results.then((rs) => {
-        //     for (const pokemon of rs) {
-        //         getData(pokemon.url).then((pokemon) => {
-        //             const thePokemonPromise = Promise.all(pokemon);
-        //             thePokemonPromise.then((poke) => console.log(poke));
-        //             // console.log(pokemon.id, pokemon.name);
-        //             container.setAttribute('id', 'container');
+        container = document.querySelector('.container');
 
-        //             const img = document.createElement('img');
-        //             const div = document.createElement('div');
-        //             div.setAttribute('id', 'pokemon-img-test');
-        //             console.log(pokemon.id, pokemon.name);
-        //             img.src = `${pokemon.sprites['other']['official-artwork'].front_default}`;
-        //             div.append(img);
-        //             container.append(div);
-        //             document.body.append(container);
-        //         });
-        //     }
-        // });
-        // console.log(resultsArray);
-        // Array.from(results).forEach((result) => console.log(result.id));
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+
         for (const pokemon of data.results) {
             getData(pokemon.url).then((pokemon) => {
-                // console.log(pokemon.id, pokemon.name, pokemon);
-                container.setAttribute('id', 'container');
-
+                const heading1 = document.createElement('h1');
+                const heading2 = document.createElement('h2');
                 const img = document.createElement('img');
                 const div = document.createElement('div');
-                div.setAttribute('id', 'pokemon-img-test');
+                div.setAttribute('class', 'pokemon-card');
                 console.log(pokemon.id, pokemon.name);
                 img.src = `${pokemon.sprites['other']['official-artwork'].front_default}`;
+                img.setAttribute('loading', 'lazy');
+                const firstLetterUpper =
+                    pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+                heading1.textContent = firstLetterUpper;
+                heading2.textContent = pokemon.id;
+                div.prepend(heading2);
+                div.prepend(heading1);
                 div.append(img);
                 container.append(div);
-                document.body.append(container);
+                // document.body.append(container);
             });
         }
+
+        console.log(container.firstChild);
     });
 });
-
-// btn.addEventListener('click', () => {
-//     getData(`${apiUrl}${pokemonLimit.value}`).then((data) => {
-//         let container;
-//         container = document.createElement('div');
-//         for (const pokemon of data.results) {
-//             getData(pokemon.url).then((pokemon) => {
-//                 // console.log(pokemon.id, pokemon.name, pokemon);
-//                 container.setAttribute('id', 'container');
-
-//                 const img = document.createElement('img');
-//                 const div = document.createElement('div');
-//                 div.setAttribute('id', 'pokemon-img-test');
-//                 console.log(
-//                     pokemon.sprites.other['official-artwork'].front_default
-//                 );
-//                 img.src = `${pokemon.sprites['other']['official-artwork'].front_default}`;
-//                 div.append(img);
-//                 container.append(div);
-//                 document.body.append(container);
-//             });
-//         }
-//     });
-// });
