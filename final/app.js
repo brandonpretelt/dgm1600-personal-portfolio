fa0d5f39f90b45af3d0251969d8895dd67caf49a
Brandon <brandonpretelt93@gmail.com>
2021-12-02 13:35:54 -0700

    :lipstick: style time

--- final/app.js
+++ final/app.js
@@ -1,7 +1,7 @@
 import { projects } from './data/projects.js';
 const projectHighlight = document.querySelector('.project-highlight');
 
-const renderCard = (title, info) => {
+const renderCard = (title, info, url) => {
     const card = document.createElement('div');
     card.className = 'card';
 
@@ -13,15 +13,18 @@ const renderCard = (title, info) => {
 
     const cardInfo = document.createElement('div');
     const cardPara = document.createElement('p');
+    const cardLink = document.createElement('a');
+    cardLink.className = 'card-link';
     cardInfo.className = 'card-info';
     cardPara.textContent = info;
-    cardInfo.append(cardPara);
-
+    cardLink.textContent = 'Link to Project';
+    cardLink.href = url;
+    cardInfo.append(cardPara, cardLink);
     card.append(cardTitle, cardInfo);
     projectHighlight.append(card);
 };
 
 projects.forEach((project) => {
-    renderCard(project.name, project.description);
-    console.log(project.name, project.description);
+    renderCard(project.name, project.description, project.url);
+    console.log(project.name, project.description, project.url);
 });
--- final/data/projects.js
+++ final/data/projects.js
@@ -1,7 +1,28 @@
 export const projects = [
     {
-        name: 'Star Wars',
-        description: 'Some star wars stuff, cool huh?',
+        name: 'Star Wars Landing Page',
+        description: 'A Cool Star Wars Landing Page',
+        url: '/starwars/index.html'
+    },
+    {
+        name: 'Star Wars Films',
+        description: 'A Star Wars Films viewer',
+        url: '/films/index.html'
+    },
+
+    {
+        name: 'Star Wars Characters',
+        description: 'A Star Wars Characters Viewer',
         url: '/sw-chars/index.html'
+    },
+    {
+        name: 'Congress',
+        description: 'A Congress Members Viewer',
+        url: '/congress/index.html'
+    },
+    {
+        name: 'Pokemon',
+        description: 'A Pokemon API Project',
+        url: '/pokemon/index.html'
     }
 ];
