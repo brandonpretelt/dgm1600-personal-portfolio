2154913951af8d8667058012f35aff8a0fce291d
Brandon <brandonpretelt93@gmail.com>
2021-12-02 19:11:35 -0700

    :hammer:

--- final/app.js
+++ final/app.js
@@ -1,30 +1,17 @@
 import { projects } from './data/projects.js';
+import { renderCard } from './final-utils/index.js';
 const projectHighlight = document.querySelector('.project-highlight');
+const projectsBtn = document.querySelector('button');
 
-const renderCard = (title, info, url) => {
-    const card = document.createElement('div');
-    card.className = 'card';
-
-    const cardTitle = document.createElement('div');
-    const cardH2 = document.createElement('h2');
-    cardTitle.className = 'card-title';
-    cardH2.textContent = title;
-    cardTitle.append(cardH2);
-
-    const cardInfo = document.createElement('div');
-    const cardPara = document.createElement('p');
-    const cardLink = document.createElement('a');
-    cardLink.className = 'card-link';
-    cardInfo.className = 'card-info';
-    cardPara.textContent = info;
-    cardLink.textContent = 'Link to Project';
-    cardLink.href = url;
-    cardInfo.append(cardPara, cardLink);
-    card.append(cardTitle, cardInfo);
-    projectHighlight.append(card);
-};
+projectsBtn.addEventListener('click', () => {
+    window.location = '#projects';
+});
 
 projects.forEach((project) => {
-    renderCard(project.name, project.description, project.url);
-    console.log(project.name, project.description, project.url);
+    renderCard(
+        project.name,
+        project.description,
+        project.url,
+        projectHighlight
+    );
 });
