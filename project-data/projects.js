90d640be82314be7c34ccaad3c007c00d6140a42
Brandon <brandonpretelt93@gmail.com>
2021-12-14 09:38:28 -0700

    :update:

--- project-data/projects.js
+++ project-data/projects.js
@@ -3,31 +3,31 @@ export const projects = [
         name: 'First Portfolio Upgrade',
         description: 'A Cool Star Wars Landing Page',
         url: '/old/index.html',
-        imgUrl: '/imgs/landing-page.png'
+        imgUrl: '/imgs/portfolio.png'
     },
     {
         name: 'SW Films',
         description: 'A Star Wars Films viewer',
         url: '/films/index.html',
-        imgUrl: '/imgs/landing-page.png'
+        imgUrl: '/imgs/films.png'
     },
 
     {
         name: 'SW Characters',
         description: 'A Star Wars Characters Viewer',
         url: '/sw-chars/index.html',
-        imgUrl: '/imgs/landing-page.png'
+        imgUrl: '/imgs/chars.png'
     },
     {
         name: 'Congress',
         description: 'A Congress Members Viewer',
         url: '/congress/index.html',
-        imgUrl: '/imgs/landing-page.png'
+        imgUrl: '/imgs/congress.png'
     },
     {
         name: 'Pokemon',
         description: 'A Pokemon API Project',
         url: '/pokemon/index.html',
-        imgUrl: '/imgs/landing-page.png'
+        imgUrl: '/imgs/pokemon.png'
     }
 ];
